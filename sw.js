ac6a3c09d31b60baaa841e606edf7901f34ae043
tatuvoardor-pixel <tatuvoardor@gmail.com>
2026-06-27 12:38:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* WORK Service Worker v0.3 */
-const CACHE='work-v0.3';
+/* WORK Service Worker v0.5.1 */
+const CACHE='work-v0.5.1';
 const FILES=['./','./index.html','./bio-bridge.js'];
 
 self.addEventListener('install',e=>{
@@ -9,23 +9,17 @@ self.addEventListener('install',e=>{
 
 self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k))))
       .then(()=>self.clients.claim())
   );
 });
 
 self.addEventListener('fetch',e=>{
-  const url=e.request.url;
-  const isCodigo=url.endsWith('.html')||url.endsWith('.js')||url.endsWith('/');
-  if(isCodigo){
-    e.respondWith(
-      fetch(e.request).then(r=>{
-        const c=r.clone();
-        caches.open(CACHE).then(cache=>cache.put(e.request,c));
-        return r;
-      }).catch(()=>caches.match(e.request))
-    );
-  }else{
-    e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));
-  }
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const c=r.clone();
+      caches.open(CACHE).then(cache=>cache.put(e.request,c));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
 });
